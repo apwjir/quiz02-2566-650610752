35d960dbff4d6f014f6ea721cccd7cf3e3083903
Jiraphat <jiraphat_po@cmu.ac.th>
2023-08-10 15:31:18 +0700

    .

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,14 +1,44 @@
+import { Reply } from "./Reply";
+import { comments } from "@/libs/comments";
 export const Comment = ({
   userImagePath,
-  username,
+  username, 
   commentText,
   likeNum,
   replies,
 }) => {
+  const replie = replies;
   return (
-    <div>
-      your code here...
-      {/* map-loop render Reply component here */}
-    </div>
+    <div className="d-flex gap-2 my-2">
+          <img
+            src={"/profileImages/" + (userImagePath) + ".jpg"}
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#3A3B3C" }}
+          >
+            <span className="fw-semibold" style={{ color: "#E4E6EB" }}>
+            {username}
+            </span>
+            <br />
+            <span style={{ color: "#E4E6EB" }}>{commentText}</span>
+            <div className="d-flex align-items-center gap-1">
+              <img src="/like.svg" width={20}></img>
+              <span style={{ color: "#B0B3B8" }}>{likeNum} คน</span>
+            </div>
+          </div>
+          {comments.map((comment) => (
+          <Reply
+          userImagePath={comment.replies.userImagePath}
+          username={comment.replies.username} 
+          commentText={comment.replies.commentText}  
+          likeNum={comment.replies.likeNum}
+          />
+          ))}
+        </div>
   );
 };
